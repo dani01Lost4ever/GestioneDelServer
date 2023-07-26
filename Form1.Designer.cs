3e24b1b9df1ab5b6cc02d2f9a635f38246bbcce6
Daniel <daniel.busetto.db@gmail.com>
2023-07-26 09:16:31 +0200

    Aggiungere i file di progetto.

---
+++ Form1.Designer.cs
@@ -0,0 +1,140 @@
+﻿namespace Spostamento_file
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variabile di progettazione necessaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Pulire le risorse in uso.
+        /// </summary>
+        /// <param name="disposing">ha valore true se le risorse gestite devono essere eliminate, false in caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Codice generato da Progettazione Windows Form
+
+        /// <summary>
+        /// Metodo necessario per il supporto della finestra di progettazione. Non modificare
+        /// il contenuto del metodo con l'editor di codice.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.buttonScegliOrigine = new System.Windows.Forms.Button();
+            this.textBoxFolderOrigine = new System.Windows.Forms.TextBox();
+            this.folderBrowserDialogOrigine = new System.Windows.Forms.FolderBrowserDialog();
+            this.openFileDialog = new System.Windows.Forms.OpenFileDialog();
+            this.folderBrowserDialogDestinazione = new System.Windows.Forms.FolderBrowserDialog();
+            this.buttonDestinazione = new System.Windows.Forms.Button();
+            this.textBoxDestinazione = new System.Windows.Forms.TextBox();
+            this.buttonSposta = new System.Windows.Forms.Button();
+            this.FileList = new System.Windows.Forms.TextBox();
+            this.buttonCopia = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // buttonScegliOrigine
+            // 
+            this.buttonScegliOrigine.Location = new System.Drawing.Point(47, 63);
+            this.buttonScegliOrigine.Name = "buttonScegliOrigine";
+            this.buttonScegliOrigine.Size = new System.Drawing.Size(280, 47);
+            this.buttonScegliOrigine.TabIndex = 0;
+            this.buttonScegliOrigine.Text = "Scegli origine";
+            this.buttonScegliOrigine.UseVisualStyleBackColor = true;
+            // 
+            // textBoxFolderOrigine
+            // 
+            this.textBoxFolderOrigine.Location = new System.Drawing.Point(359, 63);
+            this.textBoxFolderOrigine.Multiline = true;
+            this.textBoxFolderOrigine.Name = "textBoxFolderOrigine";
+            this.textBoxFolderOrigine.Size = new System.Drawing.Size(592, 47);
+            this.textBoxFolderOrigine.TabIndex = 2;
+            // 
+            // openFileDialog
+            // 
+            this.openFileDialog.FileName = "openFileDialog1";
+            // 
+            // buttonDestinazione
+            // 
+            this.buttonDestinazione.Location = new System.Drawing.Point(47, 116);
+            this.buttonDestinazione.Name = "buttonDestinazione";
+            this.buttonDestinazione.Size = new System.Drawing.Size(280, 49);
+            this.buttonDestinazione.TabIndex = 4;
+            this.buttonDestinazione.Text = "buttonSelezionaDestinazione";
+            this.buttonDestinazione.UseVisualStyleBackColor = true;
+            // 
+            // textBoxDestinazione
+            // 
+            this.textBoxDestinazione.Location = new System.Drawing.Point(359, 116);
+            this.textBoxDestinazione.Multiline = true;
+            this.textBoxDestinazione.Name = "textBoxDestinazione";
+            this.textBoxDestinazione.Size = new System.Drawing.Size(592, 49);
+            this.textBoxDestinazione.TabIndex = 5;
+            // 
+            // buttonSposta
+            // 
+            this.buttonSposta.Location = new System.Drawing.Point(47, 339);
+            this.buttonSposta.Name = "buttonSposta";
+            this.buttonSposta.Size = new System.Drawing.Size(904, 49);
+            this.buttonSposta.TabIndex = 6;
+            this.buttonSposta.Text = "buttonSpostaToDestinazione";
+            this.buttonSposta.UseVisualStyleBackColor = true;
+            // 
+            // FileList
+            // 
+            this.FileList.Location = new System.Drawing.Point(47, 394);
+            this.FileList.Multiline = true;
+            this.FileList.Name = "FileList";
+            this.FileList.Size = new System.Drawing.Size(904, 90);
+            this.FileList.TabIndex = 7;
+            // 
+            // buttonCopia
+            // 
+            this.buttonCopia.Location = new System.Drawing.Point(47, 284);
+            this.buttonCopia.Name = "buttonCopia";
+            this.buttonCopia.Size = new System.Drawing.Size(904, 49);
+            this.buttonCopia.TabIndex = 8;
+            this.buttonCopia.Text = "buttonCopiaToDestinazione";
+            this.buttonCopia.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(984, 496);
+            this.Controls.Add(this.buttonCopia);
+            this.Controls.Add(this.FileList);
+            this.Controls.Add(this.buttonSposta);
+            this.Controls.Add(this.textBoxDestinazione);
+            this.Controls.Add(this.buttonDestinazione);
+            this.Controls.Add(this.textBoxFolderOrigine);
+            this.Controls.Add(this.buttonScegliOrigine);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button buttonScegliOrigine;
+        private System.Windows.Forms.TextBox textBoxFolderOrigine;
+        private System.Windows.Forms.FolderBrowserDialog folderBrowserDialogOrigine;
+        private System.Windows.Forms.OpenFileDialog openFileDialog;
+        private System.Windows.Forms.FolderBrowserDialog folderBrowserDialogDestinazione;
+        private System.Windows.Forms.Button buttonDestinazione;
+        private System.Windows.Forms.TextBox textBoxDestinazione;
+        private System.Windows.Forms.Button buttonSposta;
+        private System.Windows.Forms.TextBox FileList;
+        private System.Windows.Forms.Button buttonCopia;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Spostamento_file
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            this.buttonScegliOrigine.Click += ButtonScegliOrigine_Click;
+            this.buttonDestinazione.Click += ButtonDestinazione_Click;
+            this.buttonSposta.Click += ButtonSposta_Click;
+            this.buttonCopia.Click += ButtonCopia_Click;
+        }
+
+        private void ButtonCopia_Click(object sender, EventArgs e)
+        {
+            if (System.IO.Directory.Exists(textBoxFolderOrigine.Text) && System.IO.Directory.Exists(textBoxDestinazione.Text))
+            {
+
+                string[] files = System.IO.Directory.GetFiles(textBoxFolderOrigine.Text);
+
+                StringBuilder fileListbuilder = new StringBuilder();
+
+                foreach (String pathSingleFile in files)
+                {
+                    fileListbuilder.Append(" ").Append(pathSingleFile);
+                    string fileName = System.IO.Path.GetFileNameWithoutExtension(pathSingleFile);
+                    string fileExt = System.IO.Path.GetExtension(pathSingleFile);
+
+                    string destFile = System.IO.Path.Combine(textBoxDestinazione.Text, fileName + fileExt);
+
+                    if (System.IO.File.Exists(destFile))
+                    {
+                        //string tempFileName = string.Format("{0}_{1}", fileName, DateTime.Now.ToString("yyyyMMddHHmmssfff"));
+                        string tempFileName = fileName + DateTime.Now.ToString("yyyyMMddHHmmssfff");
+                        destFile = System.IO.Path.Combine(textBoxDestinazione.Text, tempFileName + fileExt);
+                    }
+
+                    System.IO.File.Copy(pathSingleFile, destFile);
+                    //FileList.Text += "Done, "+ fileExt;
+                }
+            }
+            else
+            {
+                MessageBox.Show("il percorso di origine e/o destinazione non esiste");
+            }
+        }
+
+        private void ButtonSposta_Click(object sender, EventArgs e)
+        {
+            if (System.IO.Directory.Exists(textBoxFolderOrigine.Text) && System.IO.Directory.Exists(textBoxDestinazione.Text))
+            {
+                string[] files = System.IO.Directory.GetFiles(textBoxFolderOrigine.Text);
+                foreach (String pathSingleFile in files)
+                {
+                    string fileName = System.IO.Path.GetFileNameWithoutExtension(pathSingleFile);
+                    string fileExt = System.IO.Path.GetExtension(pathSingleFile);
+
+                    string destFile = System.IO.Path.Combine(textBoxDestinazione.Text, fileName + fileExt);
+
+                    int count = 1;
+                    while (System.IO.File.Exists(destFile))
+                    {
+                        string tempFileName = string.Format("{0}({1})", fileName, count++);
+                        destFile = System.IO.Path.Combine(textBoxDestinazione.Text, tempFileName + fileExt);
+                    }
+
+                    System.IO.File.Move(pathSingleFile, destFile);
+                    FileList.Text += "Done, " + fileExt;
+                }
+            }
+            else
+            {
+                MessageBox.Show("il percorso di origine e/o destinazione non esiste");
+            }
+        }
+
+        private void ButtonDestinazione_Click(object sender, EventArgs e)
+        {
+            if (folderBrowserDialogDestinazione.ShowDialog() == DialogResult.OK)
+            {
+                textBoxDestinazione.Text = folderBrowserDialogDestinazione.SelectedPath; textBoxDestinazione.Focus();
+            }
+        }
+
+        private void ButtonScegliOrigine_Click(object sender, EventArgs e)
+        {
+            if (folderBrowserDialogOrigine.ShowDialog() == DialogResult.OK)
+            {
+                textBoxFolderOrigine.Text = folderBrowserDialogOrigine.SelectedPath; textBoxFolderOrigine.Focus();
+            }
+        }
+    }
+}
